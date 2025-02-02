c6dcb47182d6ecd451683ae2c1ede9ca2f0be388
ZODIAC <melenteevvadim@gmail.com>
2025-02-02 14:27:45 +0300

    added new Generic for Dasha

--- Cpplrn.cpp
+++ Cpplrn.cpp
@@ -2,8 +2,39 @@
 #include <vector>
 using namespace std;
 
+template <typename T>
+class Dictionary {
+public:
+	Dictionary(T value) {
+		this->value = value;
+	}
+
+	T* TryGetValueByKey(string key) {
+		return ContainKey(key) ? &value : nullptr;
+	}
+
+	void AddKey(string key) {
+		if (!ContainKey(key))
+			keys.push_back(key);
+	}
+
+private:
+	bool ContainKey(string key) {
+		for (size_t i{}; i < keys.size(); i++)
+			if (keys[i] == key)
+				return true;
+		return false;
+	}
+
+	T value;
+	vector<string> keys;
+};
 
 int main()
 {
-	
+	Dictionary<string> dict = Dictionary<string>("VaDa");
+	dict.AddKey("Dasha");
+	dict.AddKey("Vadim");
+
+	cout << dict.TryGetValueByKey("SerGay") << endl << *dict.TryGetValueByKey("Dasha") << endl;
 }
